68e5730d58fd0bf7eab2d547ac91259401f33919
Student <example@example.com>
2019-10-31 12:05:26 -0700

    show initial stages of logic, and interface

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+var arrRomans = ["M", "D", "C", "L", "X", "V", "I"];
+var arrValues = ["1000", "500", "100", "50","10", "5", "1"];
+var arrRomanized = [];
+
+function romanConvert(userInput) {
+  console.log(userInput);
+  if (userInput < 1 || userInput > 3999 || userInput === NaN) {
+    alert("not a number");
+  } else if (userInput === 4)
+    arrRomanized.push(arrRomans[6]);
+    arrRomanized.push(arrRomans[5]);
+    console.log(arrRomanized);
+};
+
+
+
+$(document).ready(function() {
+  $("#inputForm").submit(function(event) {
+    event.preventDefault();
+    var userInput = parseInt($("#userInput").val());
+    var results = romanConvert(userInput);
+  });
+});

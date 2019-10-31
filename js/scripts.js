9e035e3157beef7b773250a4edd1389767949f65
Student <example@example.com>
2019-10-31 14:16:02 -0700

    have updates to current logic and branching. Trying out while.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,29 @@
 var arrRomans = ["M", "D", "C", "L", "X", "V", "I"];
-var arrValues = ["1000", "500", "100", "50","10", "5", "1"];
+var arrValues = [1000, 500, 100, 50, 10, 5, 1];
 var arrRomanized = [];
 
 function romanConvert(userInput) {
   console.log(userInput);
+   for (i=0; i < arrRomans.length; i++);
+    if (userInput > 1000)
+      userInput - 1000;
+
+
   if (userInput < 1 || userInput > 3999 || userInput === NaN) {
     alert("not a number");
-  } else if (userInput === 4)
+
+  } else if (userInput === 4){
     arrRomanized.push(arrRomans[6]);
     arrRomanized.push(arrRomans[5]);
     console.log(arrRomanized);
-};
+  } else if
+    (userInput === 9) {
+    arrRomanized.push(arrRomans[6]);
+    arrRomanized.push(arrRomans[4]);
+    console.log(arrRomanized);
 
+  };
+};
 
 
 $(document).ready(function() {
@@ -19,5 +31,5 @@ $(document).ready(function() {
     event.preventDefault();
     var userInput = parseInt($("#userInput").val());
     var results = romanConvert(userInput);
-  });
+  })
 });
